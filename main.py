342f9609b071509eecadd99cf086df02daccd99d
csalva <cristinaasalva@gmail.com>
2014-06-13 18:32:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 __author__ = 'cristina'
 
 from coches import Coche
+from transacciones import Alquiler
 
 def menu():
 	print("Barbie alquiler de coches")
@@ -17,23 +18,50 @@ opcion = str(input())
 
 if opcion == "1":
 	print("Has seleccionado alquilar un coche")
+	print("----------------------------------")
+	with open('coches.txt', mode='r', encoding='utf-8')as archivo:
+		for i in archivo:
+			matricula,marca,modelo,precioDia,disponible = i.split('-',4);
+			print(matricula,marca,modelo,precioDia,disponible)
+	print("Elige un modelo y introduce la matricula del coche")
+	matricula2 = str(input())
+	if matricula2 == matricula and disponible == "si":
+		print("Introduce el nif del cliente")
+		nif = str(input())
+		print("Introduce la fecha de alquiler")
+		fecha_alquiler = str(input())
+		print("Introduce la fecha de retorno")
+		fecha_retorno = str(input())
+		
+		transacciones = Alquiler(matricula,nif,fecha_alquiler,fecha_retorno)
+		transacciones.altaAlquiler()
+		transacciones.calcularImporte()
 	
+	else:
+		print("Matricula mal introducida")
+		
 
 if opcion == "2":
 	print("Has seleccionado ver los coches disponibles")
 	print("-------------------------------------------")
 	with open('coches.txt', mode='r', encoding='utf-8')as archivo:
 		for i in archivo:
-			matricula,marca,modelo,precioDia,disponible = i.split('-',4);
-			if disponible == "si":
+			matricula,marca,modelo,precioDia,disponible = i.split('-',5);
+			if disponible == "SI":
 				print(matricula,marca,modelo,precioDia,disponible)
 			else:
-				print("Error")
+				print(disponible+'z')
 					
 if opcion == "3":
 	print("Has seleccionado listar los coches alquilados en este momento")
 	print("-------------------------------------------------------------")
-	
+	with open('coches.txt', mode='r', encoding='utf-8')as archivo:
+		for i in archivo:
+			matricula,marca,modelo,precioDia,disponible = i.split('-',5);
+			if disponible == "NO":
+				print(matricula,marca,modelo,precioDia,disponible)
+			else:
+				print("Error")
 
 if opcion == "4":
 	print("Has seleccionado añadir un coche")
@@ -52,7 +80,12 @@ if opcion == "4":
 	coches.altaCoche()
 		
 if opcion == "5": 
-	print("Has seleccionado conocer los ingresos totales en un periodo de fechas")  
+	print("Has seleccionado conocer los ingresos totales en un periodo de fechas") 
+	print("---------------------------------------------------------------------")
+	print("Introduce la fecha 1")
+	fecha1 = str(input()) 
+	print("Introduce la fecha 2")
+	fecha2 = str(input())
     
 if opcion == "6":
 		print("Adios")
